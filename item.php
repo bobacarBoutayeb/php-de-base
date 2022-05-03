75f8571a1fffce91e6d1f0bfeea974c52164b06e
bobacar.boutayeb <bobacar.boutayeb@le-campus-numerique.fr>
2022-05-03 10:01:39 +0200

    2.2 - Template

--- item.php
+++ item.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once 'header.php';
+
 $name = "Produit local";
 $price = 0;
 $url = "https://picsum.photos/200/300";
@@ -7,3 +9,5 @@ $url = "https://picsum.photos/200/300";
 echo '<div style="display: flex; flex-direction: column; align-items: center"><h2>';
 echo $name . '</h2>' . PHP_EOL . '<h3>Prix : ' . $price . '</h3>'. PHP_EOL . '<h4>Illustration' . '</h4>' . PHP_EOL;
 echo '<img src="' . $url . '"></div>';
+
+include_once 'footer.php';
