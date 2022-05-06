3808138d1983d3c753a3a45341073f6d979174c6
bobacar.boutayeb <bobacar.boutayeb@le-campus-numerique.fr>
2022-05-06 15:10:07 +0200

    7.1 - Input for Orders

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -20,16 +20,20 @@ $products = [
 ];
 ?>
 
-    <div>
+<div>
     <?php foreach ($products as $product): ?>
-    <h3> <?php echo $product["name"] ?> </h3>
-    <p> Prix : <?php echo formatPrice($product["price"]) ?> TTC</p>
-    <p> Prix HT : <?php echo formatPrice(priceExcludingVAT($product["price"])) ?> HT</p>
-    <p> Poids : <?php echo $product["weight"] ?> kg</p>
-    <img src =" <?php echo $product["picture_url"] ?> ">
-    <p> Remise : <?php echo $product["discount"] ?>%</p>
-    </div>
-    <?php endforeach;
-
+        <h3> <?php echo $product["name"] ?> </h3>
+        <p> Prix : <?php echo formatPrice($product["price"]) ?> TTC</p>
+        <p> Prix HT : <?php echo formatPrice(priceExcludingVAT($product["price"])) ?> HT</p>
+        <p> Poids : <?php echo $product["weight"] ?> kg</p>
+        <img src=" <?php echo $product["picture_url"] ?> " alt="<?php echo $product["name"] ?>">
+        <p> Remise : <?php echo $product["discount"] ?>%</p>
+        <form method="post">
+            Quantité : <input type="number" name="post_name" value="0" style="width: 4rem">
+            <input type="submit" value="Commander">
+        </form>
+    <?php endforeach; ?>
+</div>
+<?php
 include_once 'footer.php';
 ?>
